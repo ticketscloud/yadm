f9941813cc45e596cc2cca99e16ab576e736c13f
Alexander Zelenyak <zzz.sochi@gmail.com>
2014-03-19 17:44:17 +0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,30 +2,28 @@ from setuptools import setup
 
 # python setup.py sdist --formats=bztar
 
-from structures import __version__
-
 description = 'Yet Another Document Mapper (ODM) for MongoDB'
 long_description = open('README.rst', 'rb').read().decode('utf8')
 
 
 setup(
-        name='yadm',
-        version=__version__,
-        description=description,
-        long_description=long_description,
-        author='Zelenyak Aleksander aka ZZZ',
-        author_email='zzz.sochi@gmail.com',
-        url='https://github.com/zzzsochi/yadm',
-        license='GPL',
-        platforms='any',
-        install_requires=['structires, pymongo']
+    name='yadm',
+    version='0.3',
+    description=description,
+    long_description=long_description,
+    author='Zelenyak Aleksander aka ZZZ',
+    author_email='zzz.sochi@gmail.com',
+    url='https://github.com/zzzsochi/yadm',
+    license='GPL',
+    platforms='any',
+    install_requires=['structires, pymongo']
 
-        classifiers=[
-                'Development Status :: 5 - Production/Stable',
-                'Intended Audience :: Developers',
-                'Operating System :: OS Independent',
-                'Programming Language :: Python :: 3',
-              ],
+    classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+          ],
 
-        packages=['yadm'],
-    )
+    packages=['yadm'],
+)
