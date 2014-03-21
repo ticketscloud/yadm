bc18b3a6613ce5ba3fda136a0fb2084d4c3eccc1
Alexander Zelenyak <zzz.sochi@gmail.com>
2014-03-21 14:19:38 +0400

    Create 'fields' package

---
+++ yadm/fields/__init__.py
@@ -0,0 +1,11 @@
+from yadm.fields.base import (
+    DatabaseFieldDescriptor,
+    DatabaseFieldMixin,
+    ObjectIdField,
+    BooleanField,
+    IntegerField,
+    FloatField,
+    StringField,
+)
+
+from yadm.fields.reference import ReferenceField
