68db685e1ceb26a7080cbfd3454fe8ad75a5991e
Alexander Zelenyak <zzz.sochi@gmail.com>
2014-03-25 20:08:16 +0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup
 
-from yadm import __version__
-
 # python setup.py sdist --formats=bztar
 
+version = '0.3'
 description = 'Yet Another Document Mapper (ODM) for MongoDB'
 long_description = open('README.rst', 'rb').read().decode('utf8')
 
 
 setup(
     name='yadm',
-    version=__version__,
+    version=version,
     description=description,
     long_description=long_description,
-    author='Zelenyak Aleksander aka ZZZ',
+    author='Zelenyak "ZZZ" Aleksander',
     author_email='zzz.sochi@gmail.com',
     url='https://github.com/zzzsochi/yadm',
     license='BSD',
     platforms='any',
-    install_requires=['structures, pymongo']
+    install_requires=['structures', 'pymongo']
 
     classifiers=[
             'Development Status :: 3 - Alpha',
--- yadm/__init__.py
+++ yadm/__init__.py
@@ -1,5 +1,3 @@
-__version__ = '0.3'
-
 import yadm.fields
 from yadm.database import Database
 from yadm.documents import Document, EmbeddedDocument
