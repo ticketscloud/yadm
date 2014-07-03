22b554a66e241ce3b62eefe4dc96a8921d7e9c36
Alexander Zelenyak <zzz.sochi@gmail.com>
2014-07-03 16:00:29 +0400

    Allow None in EmbeddedDocumentField

--- yadm/fields/embedded.py
+++ yadm/fields/embedded.py
@@ -33,10 +33,12 @@ class EmbeddedDocumentField(Field):
         self.embedded_document_class = embedded_document_class
 
     def prepare_value(self, value):
-        if isinstance(value, dict):
+        if value is None:
+            return None
+        elif isinstance(value, dict):
             value = self.embedded_document_class(**value)
         elif not isinstance(value, self.embedded_document_class):
-            raise TypeError('Only {!r} or dict is alowed, but {!r} given'
+            raise TypeError('Only {!r}, dict or None is alowed, but {!r} given'
                             ''.format(self.embedded_document_class, type(value)))
 
         value.__name__ = self.name
@@ -46,7 +48,10 @@ class EmbeddedDocumentField(Field):
         return to_mongo(value)
 
     def from_mongo(self, document, value):
-        if not isinstance(value, self.embedded_document_class):
+        if value is None:
+            return None
+
+        elif not isinstance(value, self.embedded_document_class):
             value = self.embedded_document_class(**value)
 
         value.__name__ = self.name
