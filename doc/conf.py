a89134801659927fea73d115a1a30a0cf4690de6
Alexander Zelenyak <zzz.sochi@gmail.com>
2015-09-25 16:57:48 +0300

    Fix version

--- doc/conf.py
+++ doc/conf.py
@@ -3,7 +3,7 @@ sys.path.insert(0, '.')
 
 project = u'yadm'
 copyright = u'2014, Alexander "ZZZ" Zelenyak'
-version = '0.4'
+version = '0.9'
 release = version
 
 extensions = ['sphinx.ext.autodoc']
