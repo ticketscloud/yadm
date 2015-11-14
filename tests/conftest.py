88c12623913c4f9ca2bfcb5faa57422de17994ec
Alexander Zelenyak <zzz.sochi@gmail.com>
2015-11-14 14:42:59 +0300

    Add '--mongo' key for py.test for setup test mongo server

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 from unittest import SkipTest
+import re
 
 import pytest
 import pymongo
@@ -6,15 +7,49 @@ import pymongo
 from yadm.database import Database
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        '--mongo',
+        action='store',
+        default='localhost:27017/test',
+        help='MongoDB connection uri',
+        metavar='"localhost:27017/test"',
+    )
+
+
 @pytest.fixture(scope='session')
-def client(request):
+def mongo_args(request):
+    uri = request.config.getoption('--mongo')
+    res = re.match(r'([\w\.]+)(?::(\d+))?(?:\/(.+))?', uri)
+
+    if not res:
+        raise ValueError(uri)
+
+    host, port, name = res.groups()
+
+    if not host:
+        host = 'localhost'
+
+    if not port:
+        port = 27017
+
+    if not name:
+        name = 'test'
+
+    return (host, int(port), name)
+
+
+@pytest.fixture(scope='session')
+def client(request, mongo_args):
+    host, port, name = mongo_args
     try:
-        return pymongo.MongoClient("localhost", 27017, tz_aware=True)
+        return pymongo.MongoClient(host, port, tz_aware=True)
     except pymongo.errors.ConnectionFailure:
         raise SkipTest("Can't connect to database (localhost:27017/test)")
 
 
 @pytest.yield_fixture(scope='function')
-def db(client):
-    client.drop_database('test')
-    yield Database(client, 'test')
+def db(client, mongo_args):
+    host, port, name = mongo_args
+    client.drop_database(name)
+    yield Database(client, name)
