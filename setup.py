84b2d60f455180f32c3906d5e8b0617bb4a8c563
Alexander Zelenyak <zzz.sochi@gmail.com>
2015-11-14 14:47:41 +0300

    Update version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 # python setup.py sdist --formats=bztar
 
-version = '1.0.0-rc1'
+version = '1.0.0'
 description = 'Yet Another Document Mapper (ODM) for MongoDB'
 long_description = open('README.rst', 'rb').read().decode('utf8')
 
@@ -18,7 +18,7 @@ setup(
     license='BSD',
     platforms='any',
     install_requires=[
-        'pymongo<3',
+        'pymongo',
         'zope.dottedname',
         'python-dateutil',
         'pytz',
@@ -26,7 +26,7 @@ setup(
     ],
 
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Topic :: Database',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
