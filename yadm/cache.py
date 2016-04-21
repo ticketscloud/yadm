ad0a2f25366dc47a66bc61b4d2a4060f2b9ef9d5
Alexander Zelenyak <zzz.sochi@gmail.com>
2016-04-21 19:36:45 +0300

    Change "# noqa" to "# pragma: no cover"

--- yadm/cache.py
+++ yadm/cache.py
@@ -3,41 +3,41 @@ import abc
 
 class CacheInterface(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def __getitem__(self, key):  # noqa
+    def __getitem__(self, key):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def __setitem__(self, key, value):  # noqa
+    def __setitem__(self, key, value):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def __delitem__(self, key):  # noqa
+    def __delitem__(self, key):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def __contains__(self, key):  # noqa
+    def __contains__(self, key):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def __len__(self, key):  # noqa
+    def __len__(self, key):  # pragma: no cover
         pass
 
 
 @CacheInterface.register
 class NotACache:
-    def __getitem__(self, key):  # noqa
+    def __getitem__(self, key):  # pragma: no cover
         raise KeyError(key)
 
-    def __setitem__(self, key, value):  # noqa
+    def __setitem__(self, key, value):  # pragma: no cover
         pass
 
-    def __delitem__(self, key):  # noqa
+    def __delitem__(self, key):  # pragma: no cover
         pass
 
-    def __contains__(self, key):  # noqa
+    def __contains__(self, key):  # pragma: no cover
         return False
 
-    def __len__(self, key):  # noqa
+    def __len__(self, key):  # pragma: no cover
         return 0
 
 
