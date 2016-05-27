beb2fbc1af759d739bed9de4fbc8ee135b3b21f2
Alexander Zelenyak <zzz.sochi@gmail.com>
2016-05-27 15:54:12 +0300

    Bump version: 1.1.0 → 1.1.1

--- doc/conf.py
+++ doc/conf.py
@@ -3,7 +3,7 @@ sys.path.insert(0, '.')
 
 project = u'yadm'
 copyright = u'2014, Alexander "ZZZ" Zelenyak'
-version = '1.1.0'
+version = '1.1.1'
 release = version
 
 extensions = ['sphinx.ext.autodoc']
